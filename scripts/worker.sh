0320e807b56b3da7a6718f9b8ad90b2939b7fadb
Your Name <you@example.com>
2025-02-24 00:18:06 +0530

    almost final

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -7,25 +7,25 @@ sudo -u hduser bash <<EOF
 sudo apt update
 sudo apt install vim ssh net-tools openjdk-8-jdk git -y
 cd ~
-wget http://172.18.4.76/hadoop-3.3.2.tar.gz
+wget http://172.20.10.2/hadoop-3.3.2.tar.gz
+# https://archive.apache.org/dist/hadoop/common/hadoop-3.3.2/hadoop-3.3.2.tar.gz
 tar xvf hadoop-3.3.2.tar.gz
 
 # Update /etc/hosts
 sudo tee -a /etc/hosts > /dev/null <<EOL
-192.168.80.100  master
-192.168.80.101  worker1
-192.168.80.102  worker2
-192.168.80.103  worker3
+192.168.10.112  master
+192.168.10.113  worker1
+192.168.10.102  worker2
+192.168.10.103  worker3
 EOL
-
 # Configure environment variables
 cat <<EOL >> ~/.bashrc
 # Hadoop and Java environment variables
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export HADOOP_HOME=\$HOME/hadoop-3.3.2
-export PATH=\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin:\$PATH
 EOL
-
+source ~/.bashrc
+echo "export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH" >> .bashrc
 source ~/.bashrc
 
 # Clone the Hadoop cluster setup repo
